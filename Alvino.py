ef0ef95a8cb11260e5d990dbfd0a5b3fc3550269
Bravo9090 <136075118+Bravo9090@users.noreply.github.com>
2023-06-25 16:37:28 +0700

    Create Alvino.py
    Update

---
+++ Alvino.py
@@ -0,0 +1,10 @@
+$ cd
+$ pkg update && apt upgrade 
+$ pkg install python git 
+$ pip install requests mechanize
+$ pip install bs4 future
+$ rm -fr cracking4
+$ git clone https://github.com/Al-Vino/cracking4
+$ ls (L kecil)
+$ cd cracking4
+$ python crack-4.py
